a00c7c1aa91f720a79bed414efb529c1055623aa
federicobaldini <federico-baldini@hotmail.com>
2022-03-08 10:02:57 +0100

    added new MenuList component

---
+++ src/home/components/MenuList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import MenuItem from "./MenuItem";
+import "./MenuList.css";
+
+const MenuList = () => {
+  return (
+    <div>
+      <h1>MenuList Works!</h1>
+      <MenuItem />
+    </div>
+  );
+};
+
+export default MenuList;
