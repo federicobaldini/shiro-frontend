7064e80a5c3a71f99c57ca653a0a608a8f430dda
federicobaldini <federico-baldini@hotmail.com>
2022-03-10 20:00:42 +0100

    added new Showcase component

---
+++ src/home/components/Showcase.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+
+import Card from "../../shared/components/UI/Card";
+import "./Showcase.css";
+
+const Showcase = (props) => {
+  const { images } = props;
+
+  const [actualIndex, setActualIndex] = useState(0);
+  const [elements, setElements] = useState(images);
+  const [transitionEnabled, setTransitionEnabled] = useState(true);
+
+  const shiftElementHandler = (offset) => {
+    let newIndex = actualIndex + offset;
+
+    setActualIndex(newIndex);
+    setTransitionEnabled(true);
+  };
+
+  const transitionEndHandler = (e) => {
+    if (e.propertyName === "transform") {
+      let temp = [...elements];
+      const lastElement = temp.shift();
+      temp.push(lastElement);
+      setElements(temp);
+      setActualIndex(0);
+      setTransitionEnabled(false);
+    }
+  };
+
+  return (
+    <div className="showcase">
+      <div
+        className="showcase-content"
+        style={{
+          transform: "translateX(-" + actualIndex * 53 + "rem)",
+          transition: !transitionEnabled ? "none" : undefined,
+        }}
+        onTransitionEnd={(e) => transitionEndHandler(e)}
+      >
+        {elements.map((element, index) => {
+          return (
+            <Card
+              key={element.id}
+              className={
+                "showcase-item" + (index === 2 ? " showcase-item-middle" : "")
+              }
+            >
+              <img
+                className="showcase-item-image"
+                src={element.path}
+                alt="product"
+              />
+            </Card>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Showcase;
--- src/home/pages/Home.js
+++ src/home/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Showcase from "../components/Showcase";
 import MenuList from "../components/MenuList";
 import "./Home.css";
 
@@ -7,32 +8,34 @@ const images = [
   {
     id: 1,
     path: "https://images.wallpaperscraft.com/image/single/sofa_furniture_interior_design_style_comfort_70000_3840x2160.jpg",
+    color: "pink",
   },
   {
     id: 2,
     path: "https://images.wallpaperscraft.com/image/single/interior_yellow_office_121457_3840x2160.jpg",
+    color: "yellow",
   },
   {
     id: 3,
     path: "https://images.wallpaperscraft.com/image/single/balcony_sofas_interior_design_apartment_room_suite_sea_ocean_landscape_penthouse_desk_tv_31421_3840x2160.jpg",
+    color: "red",
   },
   {
     id: 4,
     path: "https://images.wallpaperscraft.com/image/single/sofa_furniture_interior_design_style_comfort_70000_3840x2160.jpg",
+    color: "blue",
   },
   {
     id: 5,
     path: "https://images.wallpaperscraft.com/image/single/interior_yellow_office_121457_3840x2160.jpg",
-  },
-  {
-    id: 6,
-    path: "https://images.wallpaperscraft.com/image/single/balcony_sofas_interior_design_apartment_room_suite_sea_ocean_landscape_penthouse_desk_tv_31421_3840x2160.jpg",
+    color: "green",
   },
 ];
 
 const Home = () => {
   return (
     <div className="home">
+      <Showcase images={images} />
       <MenuList />
     </div>
   );
