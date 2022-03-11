adf91ddec325a7a33488a838798a869ab6bd3667
federicobaldini <federico-baldini@hotmail.com>
2022-03-11 13:14:15 +0100

    improved Showcase component

--- src/home/components/Showcase.js
+++ src/home/components/Showcase.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Card from "../../shared/components/UI/Card";
 import "./Showcase.css";
@@ -9,21 +9,56 @@ const Showcase = (props) => {
   const [actualIndex, setActualIndex] = useState(0);
   const [elements, setElements] = useState(images);
   const [transitionEnabled, setTransitionEnabled] = useState(false);
+  const [shiftRight, setShiftRight] = useState(false);
+  const [shiftLeft, setShiftLeft] = useState(false);
 
   const shiftElementHandler = (offset) => {
     let newIndex = actualIndex + offset;
-
+    if (offset < 0) {
+      setShiftRight(true);
+      setShiftLeft(false);
+    } else {
+      setShiftRight(false);
+      setShiftLeft(true);
+    }
     setActualIndex(newIndex);
     setTransitionEnabled(true);
+  };
+
+  const getNewElementId = (oldId) => {
+    let newId = 0;
+    if (oldId <= images.length) {
+      newId = oldId + images.length;
+    }
+    if (oldId > images.length) {
+      newId = oldId - images.length;
+    }
+    return newId;
   };
 
   const transitionEndHandler = (e) => {
     if (e.propertyName === "transform") {
       let temp = [...elements];
-      const lastElement = temp.shift();
-      temp.push(lastElement);
-      setElements(temp);
-      setActualIndex(0);
+      if (shiftRight) {
+        const lastElement = temp.shift();
+        temp.push({
+          id: getNewElementId(lastElement.id),
+          path: lastElement.path,
+          color: lastElement.color,
+        });
+        setElements(temp);
+        setActualIndex(0);
+      }
+      if (shiftLeft) {
+        const firstElement = temp.pop();
+        temp.unshift({
+          id: getNewElementId(firstElement.id),
+          path: firstElement.path,
+          color: firstElement.color,
+        });
+        setElements(temp);
+        setActualIndex(0);
+      }
       setTransitionEnabled(false);
     }
   };
@@ -33,7 +68,7 @@ const Showcase = (props) => {
       <div
         className="showcase-content"
         style={{
-          transform: "translateX(-" + actualIndex * 53 + "rem)",
+          transform: "translateX(" + actualIndex * 53 + "rem)",
           transition: !transitionEnabled ? "none" : undefined,
         }}
         onTransitionEnd={(e) => transitionEndHandler(e)}
@@ -45,13 +80,15 @@ const Showcase = (props) => {
               className={
                 "showcase-item" +
                 (!transitionEnabled
-                  ? index === 2
-                    ? " showcase-item-middle"
-                    : ""
-                  : "") +
-                (index === 3 ? " showcase-item-right" : "") +
-                (index === 1 ? " showcase-item-left" : "") 
+                  ? (index === 2 ? " showcase-item-middle" : "") ||
+                    (index === 3 ? " showcase-item-right" : "") ||
+                    (index === 1 ? " showcase-item-left" : "")
+                  : "")
               }
+              onClick={() => {
+                index === 3 && shiftElementHandler(-1);
+                index === 1 && shiftElementHandler(1);
+              }}
             >
               <img
                 className="showcase-item-image"
