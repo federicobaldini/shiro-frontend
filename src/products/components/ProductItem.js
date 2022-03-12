55667c71fdced8c8fca2ad1d2fc1eb8c1499cd07
federicobaldini <federico-baldini@hotmail.com>
2022-03-12 11:51:20 +0100

    improved ProductItem component

--- src/products/components/ProductItem.js
+++ src/products/components/ProductItem.js
@@ -1,9 +1,23 @@
 import React from "react";
 
+import Card from "../../shared/components/UI/Card";
 import "./ProductItem.css";
 
-const ProductItem = () => {
-  return <h1>ProductItem Works!</h1>;
+const ProductItem = (props) => {
+  const { product } = props;
+
+  return (
+    <Card className="product-item">
+      <img
+        src={product.path}
+        alt="product"
+        style={{ width: "66%", height: "66%", padding: "1rem" }}
+      />
+      <div style={{ width: "100%", height: "33%", padding: "1rem" }}>
+        Lorem ipsum dolor sit amet, consectetur
+      </div>
+    </Card>
+  );
 };
 
 export default ProductItem;
