417d98ad92b015c947ac736655f79e164a67c787
federicobaldini <federico-baldini@hotmail.com>
2022-03-12 11:50:57 +0100

    improved ProductList component

--- src/products/components/ProductList.js
+++ src/products/components/ProductList.js
@@ -1,9 +1,18 @@
 import React from "react";
 
+import ProductItem from "./ProductItem";
 import "./ProductList.css";
 
-const ProductList = () => {
-  return <h1>ProductList Works!</h1>;
+const ProductList = (props) => {
+  const { products } = props;
+
+  return (
+    <div className="product-list">
+      {products.map((product) => {
+        return <ProductItem key={product.id} product={product} />;
+      })}
+    </div>
+  );
 };
 
 export default ProductList;
