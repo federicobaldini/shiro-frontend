b3d63c2760628c68d36c0b7292d756bb54556e21
federicobaldini <federico-baldini@hotmail.com>
2022-03-13 10:44:47 +0100

    improved ProductItem component

--- src/products/components/ProductItem.js
+++ src/products/components/ProductItem.js
@@ -1,20 +1,34 @@
 import React from "react";
 
 import Card from "../../shared/components/UI/Card";
+import Button from "../../shared/components/Form/Button";
 import "./ProductItem.css";
 
 const ProductItem = (props) => {
   const { product } = props;
 
+  const checkIsNew = () => {
+    const productDate = new Date(product.releaseDate);
+    const today = new Date();
+    return (today.getTime() - productDate.getTime()) / (1000 * 3600 * 24) < 30;
+  };
+
   return (
     <Card className="product-item">
+      <div className="product-item__info-top">
+        <div className="product-item__new">{checkIsNew() && "NEW"}</div>
+        <div className="product-item__name">{product.name}</div>
+      </div>
       <img
-        src={product.path}
+        className="product-item__image"
+        src={product.imagePath}
         alt="product"
-        style={{ width: "66%", height: "66%", padding: "1rem" }}
       />
-      <div style={{ width: "100%", height: "33%", padding: "1rem" }}>
-        {product.description}
+      <div className="product-item__info-bottom">
+        <div className="product-item__price">{product.price}</div>
+        <div className="product-item__button-container">
+          <Button className="product-item__button">BUY</Button>
+        </div>
       </div>
     </Card>
   );
