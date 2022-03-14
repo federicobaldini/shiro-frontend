5eb67b15944885f3a56b4ab511bcaed7e46403bb
federicobaldini <federico-baldini@hotmail.com>
2022-03-14 11:49:20 +0100

    modified ProductItem to link a ProductDetail

--- src/products/components/ProductItem.js
+++ src/products/components/ProductItem.js
@@ -1,7 +1,8 @@
 import React from "react";
 
-import Card from "../../shared/components/UI/Card";
-import Button from "../../shared/components/Form/Button";
+import Card from "../../shared/components/ui/Card";
+import Button from "../../shared/components/form/Button";
+import { Link } from "react-router-dom";
 import "./ProductItem.css";
 
 const ProductItem = (props) => {
@@ -15,21 +16,25 @@ const ProductItem = (props) => {
 
   return (
     <Card className="product-item">
-      <div className="product-item__info-top">
-        <div className="product-item__new">{checkIsNew() && "NEW"}</div>
-        <div className="product-item__name">{product.name}</div>
-      </div>
-      <img
-        className="product-item__image"
-        src={product.imagePath}
-        alt="product"
-      />
-      <div className="product-item__info-bottom">
-        <div className="product-item__price">{product.price}</div>
-        <div className="product-item__button-container">
-          <Button className="product-item__button">BUY</Button>
+      <Link to={"/products/" + 1}>
+        <div className="product-item__info-top">
+          <div className="product-item__new">{checkIsNew() && "NEW"}</div>
+          <div className="product-item__name">{product.name}</div>
         </div>
-      </div>
+        <div className="product-item__image-container">
+          <img
+            className="product-item__image"
+            src={product.imagePath}
+            alt="product"
+          />
+        </div>
+        <div className="product-item__info-bottom">
+          <div className="product-item__price">{product.price}</div>
+          <div className="product-item__button-container">
+            <Button className="product-item__button">BUY</Button>
+          </div>
+        </div>
+      </Link>
     </Card>
   );
 };
