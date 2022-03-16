daf7d65be94ffef015b4aa8b8fa4866088f00237
federicobaldini <federico-baldini@hotmail.com>
2022-03-16 10:31:06 +0100

    added new ColorSelector component

---
+++ src/products/components/ColorSelector.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+import Card from "../../shared/components/ui/Card";
+import "./ColorSelector.css";
+
+const ColorSelector = (props) => {
+  const { colors } = props;
+
+  const [selectedValue, setSelectedValue] = useState("");
+
+  const handleOnClick = (code) => {
+    setSelectedValue(code);
+  };
+
+  return (
+    <div className={"color-selector " + props.className}>
+      {colors.map((color) => {
+        return (
+          <Card
+            key={color.code}
+            className={
+              "color-selector__item " +
+              (selectedValue === color.code
+                ? "color-selector__item-active"
+                : "")
+            }
+            onClick={() => handleOnClick(color.code)}
+          >
+            <div
+              className="color-selector__item-color"
+              style={{ background: color.code }}
+            />
+            <div className="color-selector__item-name">{color.name}</div>
+          </Card>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ColorSelector;
