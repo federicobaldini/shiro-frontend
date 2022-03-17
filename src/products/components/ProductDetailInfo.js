ef0babbda1cfead60c5f234b3146bdfc38e2503c
federicobaldini <federico-baldini@hotmail.com>
2022-03-17 10:26:12 +0100

    added new ProductDetailInfo component

---
+++ src/products/components/ProductDetailInfo.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import Card from "../../../shared/components/ui/Card";
+import "./ProductDetailInfo.css";
+
+const ProductDetailInfo = () => {
+  return <Card className="product-detail-info"></Card>;
+};
+
+export default ProductDetailInfo;
