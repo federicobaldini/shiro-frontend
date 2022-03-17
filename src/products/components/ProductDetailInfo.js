907b181677aa4a34c81520dd87d62598432a594a
federicobaldini <federico-baldini@hotmail.com>
2022-03-17 10:33:01 +0100

    minor changes

--- src/products/components/ProductDetailInfo.js
+++ src/products/components/ProductDetailInfo.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Card from "../../../shared/components/ui/Card";
+import Card from "../../shared/components/ui/Card";
 import "./ProductDetailInfo.css";
 
 const ProductDetailInfo = () => {
