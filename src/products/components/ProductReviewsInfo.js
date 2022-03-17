4bb3fa985e32f8e6cea6aec57dda0d5064f36191
federicobaldini <federico-baldini@hotmail.com>
2022-03-17 10:26:32 +0100

    added new ProductReviewsInfo component

---
+++ src/products/components/ProductReviewsInfo.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import Card from "../../shared/components/ui/Card";
+import "./ProductReviewsInfo.css";
+
+const ProductReviewsInfo = () => {
+  return <Card className="product-reviews-info"></Card>;
+};
+
+export default ProductReviewsInfo;
