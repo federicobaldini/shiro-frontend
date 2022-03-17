39097379577ba3e8550d0c9b2fa846be1e564ed9
federicobaldini <federico-baldini@hotmail.com>
2022-03-17 10:25:53 +0100

    added new ProductShippingInfo component

---
+++ src/products/components/ProductShippingInfo.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import Card from "../../shared/components/ui/Card";
+import "./ProductShippingInfo.css";
+
+const ProductShippingInfo = () => {
+  return <Card className="product-shipping-info"></Card>;
+};
+
+export default ProductShippingInfo;
