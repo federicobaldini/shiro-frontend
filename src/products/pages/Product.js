bfe11e2f4b11d233751ec95d22cf9d9d37ba4f50
federicobaldini <federico-baldini@hotmail.com>
2022-03-18 09:50:15 +0100

    added reviews data on Product component

--- src/products/pages/Product.js
+++ src/products/pages/Product.js
@@ -12,6 +12,7 @@ import "./Product.css";
 
 const Product = () => {
   const [loadedProducts, setLoadedProducts] = useState([]);
+  const [loadedReviews, setLoadedReviews] = useState([]);
 
   const productId = useParams().productId;
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
@@ -29,8 +30,9 @@ const Product = () => {
                 "https://i.pinimg.com/originals/ff/4b/36/ff4b36b7126bcde9f47d48cd84c0d64c.png",
               ],
               name: "Wooden table",
-              price: "150$",
+              price: 150,
               releaseDate: "2022-03-01T00:00:00.000Z",
+              shortDescription: "Lorem ipsum dolor sit amet.",
               description:
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
               colors: [
@@ -42,7 +44,28 @@ const Product = () => {
                 { name: "S", value: "S" },
               ],
               quantity: 10,
+              shippingInfo: {
+                price: 5.9,
+                daysRequiredStart: 5,
+                daysRequiredEnd: 7,
+                returnPolicy: true,
+              },
+              dimensions: "19.7 x 39.4 x 29.5 cm",
+              weight: "20kg",
+              manufacturer: "SHIRO",
+              materials: "Maple Wood and Steel",
             },
+          ],
+          reviews: [
+            { id: 1, userId: 1, productId: 2, review: "", star: 5 },
+            { id: 2, userId: 2, productId: 2, review: "", star: 5 },
+            { id: 3, userId: 3, productId: 2, review: "", star: 5 },
+            { id: 4, userId: 4, productId: 2, review: "", star: 5 },
+            { id: 5, userId: 5, productId: 2, review: "", star: 4 },
+            { id: 6, userId: 6, productId: 2, review: "", star: 4 },
+            { id: 7, userId: 7, productId: 2, review: "", star: 3 },
+            { id: 8, userId: 8, productId: 2, review: "", star: 2 },
+            { id: 9, userId: 9, productId: 2, review: "", star: 1 },
           ],
         };
         /*
@@ -51,6 +74,7 @@ const Product = () => {
         );
         */
         setLoadedProducts(data.products);
+        setLoadedReviews(data.reviews);
       } catch (err) {}
     };
 
@@ -65,22 +89,25 @@ const Product = () => {
           <LoadingSpinner />
         </div>
       ) : (
-        <div className="product">
-          {loadedProducts[0] && (
+        loadedProducts[0] &&
+        loadedReviews && (
+          <div className="product">
             <ProductDetailOverview product={loadedProducts[0]} />
-          )}
-          <div className="product-info">
-            <div className="product-info__shipping">
-              <ProductShippingInfo />
-            </div>
-            <div className="product-info__details">
-              <ProductDetailInfo />
-            </div>
-            <div className="product-info__reviews">
-              <ProductReviewsInfo />
+            <div className="product-info">
+              <div className="product-info__shipping">
+                <ProductShippingInfo
+                  shippingInfo={loadedProducts[0].shippingInfo}
+                />
+              </div>
+              <div className="product-info__details">
+                <ProductDetailInfo product={loadedProducts[0]} />
+              </div>
+              <div className="product-info__reviews">
+                <ProductReviewsInfo reviews={loadedReviews} />
+              </div>
             </div>
           </div>
-        </div>
+        )
       )}
     </React.Fragment>
   );
