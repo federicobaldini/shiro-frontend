ce599fff9715fc65b8c168fe8d7536f4c289763b
federicobaldini <federico-baldini@hotmail.com>
2022-03-18 09:49:07 +0100

    improved ProductShippingInfo component

--- src/products/components/ProductShippingInfo.js
+++ src/products/components/ProductShippingInfo.js
@@ -3,8 +3,74 @@ import React from "react";
 import Card from "../../shared/components/ui/Card";
 import "./ProductShippingInfo.css";
 
-const ProductShippingInfo = () => {
-  return <Card className="product-shipping-info"></Card>;
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const ProductShippingInfo = (props) => {
+  const { shippingInfo } = props;
+  const today = new Date();
+  const deliveryDateStart = new Date();
+  const deliveryDateEnd = new Date();
+
+  deliveryDateStart.setDate(today.getDate() + shippingInfo.daysRequiredStart);
+  deliveryDateEnd.setDate(today.getDate() + shippingInfo.daysRequiredEnd);
+
+  return (
+    <Card className="product-shipping-info">
+      <div className="product-shipping-info__title">SHIPPING INFO</div>
+      <div className="product-shipping-info__detail">
+        <div className="product-shipping-info__detail-item">
+          Shipping rate:
+          <span className="product-shipping-info__detail-rate">
+            {shippingInfo.price ? " $" + shippingInfo.price + " " : "FREE "}
+          </span>
+        </div>
+        <div className="product-shipping-info__detail-item">
+          Delivery
+          {shippingInfo.daysRequiredEnd ? " between " : " "}
+          {days[deliveryDateStart.getDay()] +
+            ", " +
+            months[deliveryDateStart.getMonth()] +
+            " " +
+            deliveryDateStart.getDate()}
+          {shippingInfo.daysRequiredEnd
+            ? " and " +
+              days[deliveryDateEnd.getDay()] +
+              ", " +
+              months[deliveryDateEnd.getMonth()] +
+              " " +
+              deliveryDateEnd.getDate()
+            : ""}
+        </div>
+      </div>
+      <div className="product-shipping-info__return-policy">
+        {shippingInfo.returnPolicy &&
+          "Return policy: Eligible for Return, Refund or Replacement"}
+      </div>
+    </Card>
+  );
 };
 
 export default ProductShippingInfo;
