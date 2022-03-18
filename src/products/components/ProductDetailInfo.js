16665dec80f02ec3485796b704ef45bef9177cc6
federicobaldini <federico-baldini@hotmail.com>
2022-03-18 09:48:18 +0100

    improved ProductDetailInfo component

--- src/products/components/ProductDetailInfo.js
+++ src/products/components/ProductDetailInfo.js
@@ -3,8 +3,31 @@ import React from "react";
 import Card from "../../shared/components/ui/Card";
 import "./ProductDetailInfo.css";
 
-const ProductDetailInfo = () => {
-  return <Card className="product-detail-info"></Card>;
+const ProductDetailInfo = (props) => {
+  const { product } = props;
+
+  return (
+    <Card className="product-detail-info">
+      <div className="product-detail-info__title">PRODUCT DETAIL</div>
+      <div className="product-detail-info__description">
+        {product.shortDescription}
+      </div>
+      <div className="product-detail-info__detail-container">
+        <Card className="product-detail-info__detail">
+          Dimensions: {product.dimensions}
+        </Card>
+        <Card className="product-detail-info__detail">
+          Weight: {product.weight}
+        </Card>
+        <Card className="product-detail-info__detail">
+          Manufacturer: {product.manufacturer}
+        </Card>
+        <Card className="product-detail-info__detail">
+          Materials: {product.materials}
+        </Card>
+      </div>
+    </Card>
+  );
 };
 
 export default ProductDetailInfo;
