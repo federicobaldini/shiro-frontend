da0a1c3656e785e9622694fb79064413f5d5cc00
federicobaldini <federico-baldini@hotmail.com>
2022-03-19 10:39:24 +0100

    improved MenuList component

--- src/home/components/MenuList.js
+++ src/home/components/MenuList.js
@@ -3,10 +3,14 @@ import React from "react";
 import MenuItem from "./MenuItem";
 import "./MenuList.css";
 
-const MenuList = () => {
+const MenuList = (props) => {
+  const { categories } = props;
+
   return (
     <div className="menu-list">
-      <MenuItem />
+      {categories.map((category) => {
+        return <MenuItem key={category.id} category={category} />;
+      })}
     </div>
   );
 };
