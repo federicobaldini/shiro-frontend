be8e7633bc4b917aa9c489e3f2fa3013b5429ec9
federicobaldini <federico-baldini@hotmail.com>
2022-03-20 11:46:05 +0100

    added new MenuTabList component

---
+++ src/shared/components/Navigation/MenuTabList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./MenuTabList.css";
+
+const MenuTabList = () => {
+  return <div className="menu-tab-list">MenuTabList Works!</div>;
+};
+
+export default MenuTabList;
