8300675ee17dfc1890ad937d0ace3b4c159ea3ef
federicobaldini <federico-baldini@hotmail.com>
2022-03-20 11:45:50 +0100

    added new MenuNavigation component

---
+++ src/shared/components/Navigation/MenuNavigation.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./MenuNavigation.css";
+
+const MenuNavigation = () => {
+  return <div className="menu-navigation">MenuNavigation Works!</div>;
+};
+
+export default MenuNavigation;
