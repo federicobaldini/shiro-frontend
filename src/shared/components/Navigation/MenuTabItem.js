5ee4819a945b5762baedabe77a5b443c072546dd
federicobaldini <federico-baldini@hotmail.com>
2022-03-20 11:46:18 +0100

    added new MenuTabItem component

---
+++ src/shared/components/Navigation/MenuTabItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./MenuTabItem.css";
+
+const MenuTabItem = () => {
+  return <div className="menu-tab-item">MenuTabItem Works!</div>;
+};
+
+export default MenuTabItem;
