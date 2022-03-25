6a8f1ac3032c65e75564d5466059ddcaaea52166
federicobaldini <federico-baldini@hotmail.com>
2022-03-25 09:09:05 +0100

    added Cart route

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import Home from "./home/pages/Home";
 import MainNavigation from "./shared/components/navigation/MainNavigation";
 import Products from "./products/pages/Products";
 import Product from "./products/pages/Product";
+import Cart from "./cart/pages/Cart";
 import { AuthContext } from "./shared/context/auth-context";
+import { CartContext } from "./shared/context/cart-context";
 import { useAuth } from "./shared/hooks/auth-hook";
+import { useCart } from "./shared/hooks/cart-hook";
 
 const App = () => {
   const { token, login, logout, userId } = useAuth();
+  const { products, addProduct, removeProduct } = useCart();
 
   let routes;
 
@@ -31,6 +35,7 @@ const App = () => {
         <Route path="/products" element={<Products />} />
         <Route path="/products/:productId" element={<Product />} />
         <Route path="/:categoryId/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
         <Route path="/auth" element={<Auth />} />
         <Route path="*" element={<Home />} />
       </Routes>
@@ -47,10 +52,18 @@ const App = () => {
         logout: logout,
       }}
     >
-      <Router>
-        <MainNavigation />
-        <main>{routes}</main>
-      </Router>
+      <CartContext.Provider
+        value={{
+          products: products,
+          addProduct: addProduct,
+          removeProduct: removeProduct,
+        }}
+      >
+        <Router>
+          <MainNavigation />
+          <main>{routes}</main>
+        </Router>
+      </CartContext.Provider>
     </AuthContext.Provider>
   );
 };
